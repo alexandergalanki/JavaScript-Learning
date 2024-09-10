53efa14e79fe84eefacce0b48867f2f435ff673e
Alexander Galanki <galankialex@gmail.com>
2024-09-11 00:34:42 +0530

    Add files via upload

---
+++ Object.js
@@ -0,0 +1,15 @@
+const newObj={
+    name:"Alex",
+    gender:"Male",
+    designation:"Consultant"
+}
+
+console.log(Object.keys(newObj));
+//Op:['name','gender','designation']
+
+console.log(Object.values(newObj));
+//Op:['Alex','Male','Consultant']
+
+console.log(Object.entries(newObj));
+//Op://Op:[['name','Alex'],['gender','Male'],['designation','Consultant']]
+
