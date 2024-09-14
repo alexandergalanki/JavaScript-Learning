5d146428ff5720c1c67c4ebc618aad51fea59171
Alexander Galanki <galankialex@gmail.com>
2024-09-14 12:04:43 +0530

    Add files via upload

---
+++ ArraytoString.js
@@ -0,0 +1,10 @@
+let array=[1,2,3,4,5];
+
+const arrToString=array.toString();
+console.log(arrToString);
+
+const arrjoin=array.join(" ");
+console.log(arrjoin);
+
+const arrString=String(array);
+console.log(arrString);
