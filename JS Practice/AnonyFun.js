88ee87be0c893323f6f8272ebf38701b8114cf73
Alexander Galanki <galankialex@gmail.com>
2024-10-01 15:49:27 +0530

    Add files via upload

---
+++ JS Practice/AnonyFun.js
@@ -0,0 +1,12 @@
+function a(){
+    console.log("this is normal fun statement or expression");    
+}
+
+var b=function(){
+    console.log("this is anony function");
+}
+
+var c=function xyz(){
+    console.log("this is named function");
+}
+
