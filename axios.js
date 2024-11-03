97926623806a289d5e580c7c3f62a4c67d4bd5eb
Alexander Galanki <galankialex@gmail.com>
2024-11-03 23:26:27 +0530

    Add files via upload

---
+++ axios.js
@@ -0,0 +1,15 @@
+const url = 'https://jsonplaceholder.typicode.com/posts'
+const data = {
+  a: 10,
+  b: 20,
+};
+axios
+  .post(url, data, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json;charset=UTF-8",
+    },
+  })
+  .then(({data}) => {
+    console.log(data);
+});
